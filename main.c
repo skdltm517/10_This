e9d5e2dceb1d76bc3657926dd47e01ee8d3eb915
jeongxhj <55428319+jeongxhj@users.noreply.github.com>
2019-11-07 15:38:29 +0900

    04_call_by_value

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	int i=300;
+void swap (int x, int y){
+	int temp;
+	temp = x;
+	x =y;
+	y = temp;
+}
+void main(void){
+	int a = 3;
+	int b = 5;
+	swap(a,b);
+	printf("a:%i, b:%i \n", a, b);
 	
-	int *pi=&i;
-	char *pc = &i;
-	
-	printf("%i, %i, %i\n", i,*pi, *pc);
-	return 0;
 }
